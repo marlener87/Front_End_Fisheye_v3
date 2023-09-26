e19591cabccd421efd7294890b5dab7772d8b4d4
marlener87 <53823616+marlener87@users.noreply.github.com>
2023-09-26 12:08:19 +0200

    initiation code projet 6, page accueil quasi finie, page photographer en progrès

---
+++ scripts/pages/index.js
@@ -0,0 +1,42 @@
+const photographersSection = document.querySelector(".photographer_section");
+
+async function getPhotographers() {
+  const result = await fetch("/data/photographers.json") // pour aller chercher un fichier .json
+    //.then((res) => console.log(res));
+    //.then((res) => res.json())
+    .then((res) => {
+      //const test = 3;
+      //const toto = { cle: "valeur" };
+      //return toto;
+
+      return res.json();
+    });
+  //.then((res2) => console.log(res2));
+  //.then((data) => console.log(data));
+
+  //console.log(result);
+  return result;
+}
+
+async function init() {
+  const listPhotographers = await getPhotographers(); // récupère les données des photographes
+  displayPhotographers(listPhotographers);
+}
+
+function displayPhotographers(objectPhotographers) {
+  objectPhotographers.photographers.forEach((item) => {
+    const divCard = document.createElement("div");
+    divCard.classList.add("card");
+
+    divCard.innerHTML = `
+        <a href="#" class="lienPhotographe"><img src="../assets/photographersIDPhotos/${item.portrait} " alt="${item.name}" class="portrait">
+        <h2 class="name">${item.name}</h2></a>
+        <p class="localisation">${item.city}, ${item.country}</p>
+        <p class="tagline">${item.tagline}</p>
+        <p class="price">${item.price}€/jour</p>
+    `;
+    photographersSection.appendChild(divCard);
+  });
+}
+
+init();
